803e6c684c516bbc847e80b341ec80bc451b76da
Abhishek Khutwad <abhishek.khutwad@cuelogic.com>
2018-08-24 20:55:47 +0530

    token

--- router/update.js
+++ router/update.js
@@ -2,10 +2,16 @@ var express = require ('express');
 var router = express.Router();
 var path = require('path');
 var authentication = require('../middleware/auth');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 router.get('/',function(req,res){
     console.log("done");
     res.sendFile(path.join('/home/abhishek/NodeFinal' + '/public'+'/update.html'))
+    console.log(req.query.id);
 })
 
 module.exports=router;
